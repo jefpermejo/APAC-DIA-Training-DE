ef4cc187dbe3fc91ff472a6646e8da5aff1a8043
Jeff Permejo <Jeff.Permejo@insight.com>
2025-10-14 13:12:31 +0800

    Exercise 3: Added support for shipments data ingestion: enable Parquet file handling in bronze layer.

--- scripts/load_to_bronze.py
+++ scripts/load_to_bronze.py
@@ -31,7 +31,8 @@ tables = [
     {"name": "orders_header", "filename": "orders", "format": "csv", "schema": orders_header_schema, "write_delta": True, "partitioned": True, "file_pattern": "part-*.csv"},
     {"name": "orders_lines", "filename": "orders", "format": "csv", "schema": orders_lines_schema, "write_delta": True, "partitioned": True, "file_pattern": "order_lines.csv"},
     {"name": "sensors", "filename": "sensors", "format": "csv", "schema": sensors_schema, "write_delta": True, "partitioned": True, "file_pattern": "sensors.csv"},
-    {"name": "exchange_rates", "filename": "exchange_rates.xlsx", "format": "xlsx", "schema": exchange_rates_schema, "write_delta": True, "partitioned": False}
+    {"name": "exchange_rates", "filename": "exchange_rates.xlsx", "format": "xlsx", "schema": exchange_rates_schema, "write_delta": True, "partitioned": False},
+    {"name": "shipments", "filename": "shipments.parquet", "schema": shipments_schema, "format": "parquet", "write_delta": True, "partitioned": False, "file_pattern": "*.parquet"}
     ]
 
 # Parse arguments
@@ -90,6 +91,8 @@ def read_table_by_format(file_path, format):
     elif format == "xlsx":
         df = pd.read_excel(file_path)
         return pa.Table.from_pandas(df)
+    elif format == "parquet":
+        return pq.read_table(file_path)
     else:
         raise ValueError(f"Unsupported format: {format}")
     
