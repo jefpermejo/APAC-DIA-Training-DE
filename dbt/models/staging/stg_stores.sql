875656b70d5448de63cf3d709a9031e5ebf334b7
Jeff Permejo <Jeff.Permejo@insight.com>
2025-10-15 16:55:26 +0800

    Exercise 4: Staged and cleaned stores data

---
+++ dbt/models/staging/stg_stores.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='table', contract={'enforced': true}) }}
+
+with src as (
+  select * from {{ source('main_stg', 'stores') }}
+),
+cleaned_stores as (
+  select
+    cast(store_id as bigint) as store_id,
+    trim(store_code) as store_code,
+    trim(name) as name,
+    trim(channel) as channel,
+    trim(region) as region,
+    upper(trim(state)) as state,
+    cast(latitude as double) as latitude,
+    cast(longitude as double) as longitude,
+    cast(open_dt as date) as open_dt,
+    cast(close_dt as date) as close_dt
+  from src
+)
+select * from cleaned_stores
