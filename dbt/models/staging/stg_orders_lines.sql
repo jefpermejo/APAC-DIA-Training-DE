2ee6c47a9edb422cb5fc803ec103664781e4efd6
Jeff Permejo <Jeff.Permejo@insight.com>
2025-10-16 11:53:27 +0800

    Exercise 4: Cleaned and staged orders_lines model

---
+++ dbt/models/staging/stg_orders_lines.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='table', contract={'enforced': true}) }}
+
+with src as (
+  select * from {{ source('main_stg', 'orders_lines') }}
+),
+cleaned_orders_lines as (
+  select
+    cast(order_id as bigint) as order_id,
+    cast(line_number as int) as line_number,
+    cast(product_id as bigint) as product_id,
+    cast(qty as int) as quantity,
+    cast(unit_price as decimal(10, 4)) as unit_price,
+    cast(line_discount_pct as decimal(10, 4)) as line_discount_percent,
+    cast(tax_pct as decimal(10, 4)) as tax_percent
+  from src
+)
+select * from cleaned_orders_lines
