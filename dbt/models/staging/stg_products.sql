87415c51c4068a27e9b9912357a43750e0d16d04
Jeff Permejo <Jeff.Permejo@insight.com>
2025-10-17 15:20:50 +0800

    Exercise 4: Added enrichment to products table

--- dbt/models/staging/stg_products.sql
+++ dbt/models/staging/stg_products.sql
@@ -16,5 +16,27 @@ cleaned_products as (
     cast(introduced_dt as date) as introduced_dt,
     cast(discontinued_dt as date) as discontinued_dt
   from src
+),
+enriched_products as (
+  select 
+  *,
+  case 
+    when current_price < 20 then 'Budget'
+    when current_price between 20 and 100 then 'Mid-Range'
+    else 'Premium'
+  end as price_segment,
+  date_diff('day', introduced_dt, current_date) as days_on_market,
+  case 
+    when is_discontinued then 'Discontinued'
+    when date_diff('day', introduced_dt, current_date) <= 90 then 'New'
+    else 'Active'
+  end as product_status,
+  case
+    when category in ('ELECTRONICS', 'APPLIANCES') then 'High Value'
+    when category in ('FOOD', 'BEVERAGES') then 'Consumable'
+    else 'General Merchandise'
+  end as product_group,
+  cast(current_timestamp as timestamp) as staging_ts
+  from cleaned_products
 )
-select * from cleaned_products
+select * from enriched_products
