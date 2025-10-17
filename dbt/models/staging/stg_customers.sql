7e087d208d14abfb6b271ff4e22c7c45b9b0bff0
Jeff Permejo <Jeff.Permejo@insight.com>
2025-10-17 13:36:04 +0800

    Exercise 4: Added enrichment to customers table

--- dbt/models/staging/stg_customers.sql
+++ dbt/models/staging/stg_customers.sql
@@ -24,6 +24,37 @@ cleaned_customers as (
     cast(is_vip as boolean) as is_vip,
     cast(gdpr_consent as boolean) as gdpr_consent
   from src
+),
+
+enriched_customers as (
+  select 
+  *,
+  trim(first_name) || ' ' || trim(last_name) as full_name,
+  date_diff('year', birth_date, current_date) as age,
+  date_diff('day', join_ts_utc, current_timestamp) as days_since_join,
+  case 
+    when is_vip then 'VIP'
+    else 'Regular'
+  end as customer_segment,
+  case 
+    when gdpr_consent then 'Consented'
+    else 'Not Consented'
+  end as gdpr_status,
+  case 
+    when country_code = 'US' then 'United States'
+    when country_code = 'CA' then 'Canada'
+    when country_code = 'GB' then 'United Kingdom'
+    when country_code = 'AU' then 'Australia'
+    when country_code = 'IN' then 'India'
+    else 'Other'
+  end as country_name,
+  case 
+    when email = 'bad_email' then 'Invalid Email'
+    else regexp_replace(email, '[^a-zA-Z0-9@._-]', '')
+  end as email_clean,
+  regexp_extract(email, '@(.*)$', 1) as email_domain,
+  cast(current_timestamp as timestamp) as staging_ts
+  from cleaned_customers
 )
 
-select * from cleaned_customers
+select * from enriched_customers
