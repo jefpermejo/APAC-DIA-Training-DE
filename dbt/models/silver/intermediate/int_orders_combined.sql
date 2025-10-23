7bb2b9a82c05b9f338c568250b28980fcc0d4628
Jeff Permejo <Jeff.Permejo@insight.com>
2025-10-23 20:08:08 +0800

    Exercise 4: Refactor intermediate orders models for improved enrichment and Gold fact sales compliance

--- dbt/models/silver/intermediate/int_orders_combined.sql
+++ dbt/models/silver/intermediate/int_orders_combined.sql
@@ -5,26 +5,81 @@ with header as (
 lines as (
   select * from {{ ref('silver_orders_lines') }}
 ),
+customers as (
+  select customer_id, customer_segment from {{ ref('silver_customers') }}
+),
+products as (
+  select product_id, name as product_name, category, subcategory, price_segment, product_status, product_group from {{ ref('silver_products') }}
+),
+stores as (
+  select store_id, name as store_name, region, state, channel from {{ ref('silver_stores') }}
+),
+fx as (
+  select currency, date, rate_to_aud from {{ ref('silver_exchange_rates') }}
+),
 joined as (
   select
     h.order_id,
     h.order_ts_utc,
-    h.order_dt_local,
-    h.channel,
-    h.payment_method,
-    h.coupon_code,
-    h.shipping_fee,
-    h.currency,
     h.customer_id,
-    h.store_id,
-    l.line_number,
-    l.product_id,
-    l.quantity,
-    l.unit_price,
-    l.line_discount_percent,
-    l.tax_percent
+    c.customer_segment,
+  h.store_id,
+  s.store_name,
+  s.region,
+  s.state,
+  s.channel,
+  l.line_number,
+  l.product_id,
+  p.product_name,
+  p.category,
+  p.subcategory,
+  p.price_segment,
+  p.product_status,
+  p.product_group,
+  l.quantity,
+  l.unit_price,
+  l.line_discount_percent,
+  l.tax_percent,
+  h.currency,
+  fx.rate_to_aud,
+  l.unit_price * l.quantity as line_total_orig_ccy,
+  l.unit_price * l.quantity * fx.rate_to_aud as line_total_base_ccy,
+  h.coupon_code,
+  h.payment_method
   from header h
   inner join lines l on h.order_id = l.order_id
+  left join customers c on h.customer_id = c.customer_id
+  left join products p on l.product_id = p.product_id
+  left join stores s on h.store_id = s.store_id
+  left join fx on h.currency = fx.currency and date(h.order_ts_utc) = fx.date
 )
-select * from joined
+select
+  order_id,
+  order_ts_utc,
+  customer_id,
+  customer_segment,
+  store_id,
+  store_name,
+  region,
+  state,
+  channel,
+  line_number,
+  product_id,
+  product_name,
+  category,
+  subcategory,
+  price_segment,
+  product_status,
+  product_group,
+  quantity,
+  unit_price,
+  line_discount_percent,
+  tax_percent,
+  currency,
+  rate_to_aud,
+  line_total_orig_ccy,
+  line_total_base_ccy,
+  coupon_code,
+  payment_method
+from joined
 order by order_id, line_number
